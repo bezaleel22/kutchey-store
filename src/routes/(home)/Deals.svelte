00241fb3690e33c4299cef08b29a52e55ba2d210
Onojeta Brown <onosbrown.saved@gmail.com>
2024-08-30 16:01:58 +0100

    Deployment

--- src/routes/(home)/Deals.svelte
+++ src/routes/(home)/Deals.svelte
@@ -9,6 +9,7 @@
   let h = 0;
   let m = 0;
   let s = 0;
+  let dealend = false;
 
   onMount(() => {
     // if (!endDate) {
@@ -24,7 +25,8 @@
       let distance = future - now;
       if (distance < 0) {
         clearInterval(timer);
-        // document.getElementById("countdown").innerHTML = "EXPIRED!";
+        dealend = true;
+        console.log("done");
         return;
       }
       d = Math.floor(distance / days);
@@ -38,40 +40,40 @@
   });
 </script>
 
-<div class="flex justify-between mb-10">
-  <div class="ti">
-    <h3 class="!mb-0 section__title"><span>Deal of the</span>Day</h3>
-    <span class="leading-3">
-      Don't wait. The time will never be just right.
-    </span>
-  </div>
+{#if !dealend}
+  <div class="flex justify-between mb-10">
+    <div class="ti">
+      <h3 class="!mb-0 section__title"><span>Deal of the</span> Day</h3>
+      <span class="leading-3">
+        Don't wait. The time will never be just right.
+      </span>
+    </div>
 
-      <div class="countdown">
-        <div class="countdown__amount">
-          <p class="countdown__period">{d}</p>
-          <span class="unit">Days</span>
-        </div>
-        <div class="countdown__amount">
-          <p class="countdown__period">{h}</p>
-          <span class="unit">Hours</span>
-        </div>
-        <div class="countdown__amount">
-          <p class="countdown__period">{m}</p>
-          <span class="unit">Mins</span>
-        </div>
-        <div class="countdown__amount">
-          <p class="countdown__period">{s}</p>
-          <span class="unit">Sec</span>
-        </div>
+    <div class="countdown">
+      <div class="countdown__amount">
+        <p class="countdown__period">{d}</p>
+        <span class="unit">Days</span>
       </div>
-</div>
-
-{#if result.products}
-  <div class="products__container grid">
-    {#each result.products as product}
-      <ProductCard {product} />
-    {/each}
+      <div class="countdown__amount">
+        <p class="countdown__period">{h}</p>
+        <span class="unit">Hours</span>
+      </div>
+      <div class="countdown__amount">
+        <p class="countdown__period">{m}</p>
+        <span class="unit">Mins</span>
+      </div>
+      <div class="countdown__amount">
+        <p class="countdown__period">{s}</p>
+        <span class="unit">Sec</span>
+      </div>
+    </div>
   </div>
-{/if}
 
-<!--  Day Of The Deal Start End -->
+  {#if result.products}
+    <div class="products__container grid">
+      {#each result.products as product}
+        <ProductCard {product} />
+      {/each}
+    </div>
+  {/if}
+{/if}
